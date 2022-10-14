7c71e2074c8afc059ceeb77d44e3d2e215a84913
Striletskiy <biataru1@gmail.com>
2022-10-14 14:46:19 +0300

    task 2

--- src/main/main.java
+++ src/main/main.java
@@ -2,14 +2,18 @@ package main;
 
 import basic_java.BasicJava;
 import statements_and_loops.Task;
+import work_with_text_and_arrays.Task2;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 
 import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args){
 
-//        BasicJava basicJava = new BasicJava();
-//        basicJava.calculate();
+        BasicJava basicJava = new BasicJava();
+        basicJava.calculate();
 
         Task task = new Task();
         task.comparingTwoNumbers();
@@ -20,6 +24,20 @@ public class main {
         task.factorial(scanner);
         task.distanceBetweenTwoPoints(scanner);
         task.mathOperations(scanner);
+//        task.theSumOfTheDigits0fAnInteger();
+//        task.tenYearNumber(int decimal);
+
+        Task2 task2 = new Task2();
+        task2.qw();
+        task2.er();
+        task2.rt();
+        task2.averageValue();
+        task2.maxMin();
+        task2.equalityChecks();
+        task2.DeleteAnElemenFromAnArray();
+        task2.stringPerNumber();
+        task2.duplicateElementsInAnArray();
+
 
     }
    }
--- src/statements_and_loops/Task.java
+++ src/statements_and_loops/Task.java
@@ -7,7 +7,7 @@ public class  Task{
     public void comparingTwoNumbers() {
         int q = 24;
         int w = 10;
-        System.out.println(q == w);
+        System.out.println(q < w);
 
     }
     public void sumOfNaturalNumbers(Scanner scanner){
@@ -31,7 +31,6 @@ public class  Task{
         int result = 1;
         int k = scanner.nextInt();
         for (int i = 1; i <= k ; i++) {
-
             result = result * i;
         }
         System.out.println(result);
@@ -42,12 +41,12 @@ public class  Task{
         int x2 = scanner.nextInt();
         int y1 = scanner.nextInt();
         int y2 = scanner.nextInt();
-        float result =(float) Math.sqrt(Math.pow( x1 - x2 ,2)+Math.pow( y1 - y2 ,2));
+        double result = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
         System.out.println(result);
 
     }
     public  void  mathOperations(Scanner scanner){
-        String st = scanner.nextLine() ;
+        String st = scanner.nextLine();
         double a = scanner.nextInt();
         double b = scanner.nextInt();
         double result;
@@ -72,6 +71,29 @@ public class  Task{
                 break;
         }
         System.out.println(result);
+//    }
+//    public void theSumOfTheDigits0fAnInteger(String str) {
+//        char[] array = str.toCharArray();
+//        String result = "";
+//        for (int i = array.length - 1; i >= 0; i--) {
+//            result = result + array[i];
+//        }
+//        return result;
+//
+//    }
+//    public void tenYearNumber(int decimal){
+//        int rem;
+//        String hex="";
+//        char hexchars[]={'0','1','10','11','100','101','110','111','1000','1001','1010','1011','1100','1101','1110','1111'};
+//        while(decimal>0)
+//        {
+//            rem=decimal%2;
+//            hex=hexchars[rem]+hex;
+//            decimal=decimal/2;
+//        }
+//        return hex;
     }
+
+
 
 }
